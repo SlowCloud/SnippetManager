c318cba5845b69a77f63b05576c3764257aa1d89
slowcloud <sju9417@gmail.com>
2025-12-13 13:11:23 +0900

    초기 커밋.

---
+++ SnippetManager/Models/Snippet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnippetManager.Models
+{
+    public class Snippet
+    {
+        private int snippetId;
+        private DateTime createdAt;
+        private string content;
+
+        public Snippet(string content)
+        {
+            this.content = content;
+        }
+
+        public int SnippetId { get => snippetId; set => snippetId = value; }
+        public DateTime CreatedAt { get => createdAt; set => createdAt = value; }
+        public string Content { get => content; set => content = value; }
+    }
+}
